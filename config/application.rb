b8a66c906e37ec487b999f0e98232e3030ff4ef4
Willem Handreck <willem-h@users.noreply.github.com>
2017-03-12 20:37:47 +1000

    Use Rails 5 API only mode (#9)
    This app is only going to be an API without serving a web app so
    specifying that means that Rails is more likely to work for us.
    
    This change makes use of the new API only mode that can be used in
    Rails 5.

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ Bundler.require(*Rails.groups)
 
 module Openshelf
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.api_only = true
   end
 end
