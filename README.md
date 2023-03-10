b85b6dba4ef0e4b8ea6247d1e4a4d388105bf3b1
Willem Handreck <willem.handreck@gmail.com>
2023-03-10 08:15:25 +0000

    Add notes about REST API design

--- README.md
+++ README.md
@@ -5,12 +5,34 @@ that your friends already have.
 
 ## Scratchpad
 
+GET /users Lists all users
+POST /users Creates a new user
+
+GET /users/1 Returns user with ID 1
+PATCH /users/1 Updates user with ID 1
+DELETE /users/1 Deletes user with ID 1
+
+I'm not sure how logging in works yet. Maybe that's another resource?
+
+GET /books Lists all books
+POST /books Creates a new book
+
+GET /books/1 Returns book with ID 1.
+PATCH /books/1 Updates book with ID 1.
+DELETE /books/1 Deletes book with ID 1.
+
+GET /users/1/owns List books that user with ID 1 owns
+POST /users/1/owns Add another owned book to user with ID 1.
+
+GET /users/1/owns/1 
+
+
+
 /users/1/loans/1
-/users/1/loans?borrower_id=2  // Get a list of loans between user 1 and user 2
+/users/1/loans?borrower_id=2 Get a list of loans between user 1 and user 2.
 
 /users/1/borrows/1
-/users/1/borrows?loaner_id=2
+/users/1/borrows?loaner_id=2 Get a list of borrows between user 1 and user 2.
 
-/users/1/owns/1
 
 /users/1/wants/1
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,68 +2,70 @@ import { useState } from 'react'
 
 import Shelf from '../components/Shelf'
 
+const initialState = [
+  {
+    id: 1,
+    title: "Loaned",
+    books: [
+      {
+        id: 1,
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+      },
+      {
+        id: 2,
+        title: "Mere Christianity",
+        author: "C.S. Lewis",
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: "Borrowed",
+    books: [
+      {
+        id: 3,
+        title: "Silmarilion",
+        author: "J.R.R. Tolkien",
+      },
+    ]
+  },
+  {
+    id: 3,
+    title: "Wanted",
+    books: [
+      {
+        id: 4,
+        title: "1984",
+        author: "George Orwell",
+      },
+    ]
+  },
+  {
+    id: 4,
+    title: "Owned",
+    books: [
+      {
+        id: 5,
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+      },
+      {
+        id: 6,
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+      },
+      {
+        id: 7,
+        title: "The Martian",
+        author: "Andy Weir",
+      },
+    ]
+  }
+]
+
 function Home() {
-  const [shelves, setShelves] = useState([
-    {
-      id: 1,
-      title: "Loaned",
-      books: [
-        {
-          id: 1,
-          title: "The Hobbit",
-          author: "J.R.R. Tolkien",
-        },
-        {
-          id: 2,
-          title: "Mere Christianity",
-          author: "C.S. Lewis",
-        },
-      ]
-    },
-    {
-      id: 2,
-      title: "Borrowed",
-      books: [
-        {
-          id: 3,
-          title: "Silmarilion",
-          author: "J.R.R. Tolkien",
-        },
-      ]
-    },
-    {
-      id: 3,
-      title: "Wanted",
-      books: [
-        {
-          id: 4,
-          title: "1984",
-          author: "George Orwell",
-        },
-      ]
-    },
-    {
-      id: 4,
-      title: "Owned",
-      books: [
-        {
-          id: 5,
-          title: "Pride and Prejudice",
-          author: "Jane Austen",
-        },
-        {
-          id: 6,
-          title: "Harry Potter",
-          author: "J.K. Rowling",
-        },
-        {
-          id: 7,
-          title: "The Martian",
-          author: "Andy Weir",
-        },
-      ]
-    }
-  ])
+  const [shelves, setShelves] = useState()
 
   return (
     <div className="h-screen bg-slate-100">
