f14172a82a563d809b7e18b1ea8a8a845c893b2f
Willem Handreck <willem.handreck@gmail.com>
2023-03-27 17:09:19 +1100

    Update Lambda with code written in AWS console

--- functions/index.mjs
+++ functions/index.mjs
@@ -2,23 +2,28 @@ import pg from 'pg'
 const { Client } = pg
 
 const client = new Client({
-    host: 'openshelf.cngodqq9yemw.ap-southeast-2.rds.amazonaws.com',
-    port: 5432,
-    user: 'postgres',
-    password: 'gm8uvDHmGTKQevHQ8bZ'
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
 })
 
-client
-    .connect()
-    .then(() => console.log('connected'))
-    .catch((err) => console.error('connection error', err.stack))
+try {
+    client.connect()
+    console.log('connected')
+}
+catch (err) {
+    console.error('connection error', err.stack)
+}
 
 export const handler = async (event) => {
-    const result = await client.query('SELECT 2')
-    // const response = {
-    //     statusCode: 200,
-    //     text: "Hello 1",
-    //     body: JSON.stringify(result)
-    // }
-    return result
+    try {
+        const result = await client.query('SELECT * FROM books')
+        return result.rows
+    }
+    catch (err) {
+        console.error('query error', err.stack)
+        return {}
+    }
 }
