5569647e3329062348656fbf090c2266e5d27140
Willem Handreck <willem.handreck@gmail.com>
2023-03-30 18:51:16 +1100

    Connect to Lambda for the first time

--- web-client/src/pages/Home.tsx
+++ web-client/src/pages/Home.tsx
@@ -1,78 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Shelf from '../components/Shelf'
 
-const initialState = [
-  {
-    id: 1,
-    title: "Loaned",
-    books: [
-      {
-        id: 1,
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-      },
-      {
-        id: 2,
-        title: "Mere Christianity",
-        author: "C.S. Lewis",
-      },
-    ]
-  },
-  {
-    id: 2,
-    title: "Borrowed",
-    books: [
-      {
-        id: 3,
-        title: "Silmarilion",
-        author: "J.R.R. Tolkien",
-      },
-    ]
-  },
-  {
-    id: 3,
-    title: "Wanted",
-    books: [
-      {
-        id: 4,
-        title: "1984",
-        author: "George Orwell",
-      },
-    ]
-  },
-  {
-    id: 4,
-    title: "Owned",
-    books: [
-      {
-        id: 5,
-        title: "Pride and Prejudice",
-        author: "Jane Austen",
-      },
-      {
-        id: 6,
-        title: "Harry Potter",
-        author: "J.K. Rowling",
-      },
-      {
-        id: 7,
-        title: "The Martian",
-        author: "Andy Weir",
-      },
-    ]
-  }
-]
-
 function Home() {
-  const [shelves, setShelves] = useState(initialState)
+  const [books, setBooks] = useState([])
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch('https://wxt9njnxk9.execute-api.ap-southeast-2.amazonaws.com/dev/books')
+      const data = await response.json()
+      setBooks(data)
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
 
   return (
     <div className="h-screen bg-slate-100">
 
-      {shelves.map(({ id, title, books }) => (
-        <Shelf id={id} name={title} books={books} />
-      ))}
+      <Shelf id={1} name={'Books'} books={books} />
 
     </div>
   )
